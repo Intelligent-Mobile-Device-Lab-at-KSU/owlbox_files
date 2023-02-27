f36b431b88bc33d3360f2930c463dcecacc3f5ea
scrappylabs <57416600+scrappylabs@users.noreply.github.com>
2023-02-27 12:31:02 -0500

    Create pyrx.py

---
+++ test/pyrx.py
@@ -0,0 +1,13 @@
+import sys
+import socket
+ETH_P_ALL=0x88DC # not defined in socket module, sadly...
+s=socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
+s.bind(("wlan0", 0))
+pktnum = 1
+
+#sys.stdout.write("<%s>\n"%repr(r))
+while True:
+    r=s.recv(2000)
+    print("Received packet %i" % pktnum)
+    pktnum = pktnum+1
+    #print("%s" % r)
