772c7e76ac8c32be36e2c0ad63448e22b1a53981
scrappylabs <57416600+scrappylabs@users.noreply.github.com>
2023-05-22 07:10:08 -0400

    Update and rename install.sh to owlbox-install-v2x.sh

---
+++ owlbox-install-v2x.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Changing to /home/pi/owlbox_files"
+cd /home/pi/owlbox_files
+echo "Performing git pull..."
+git pull
+wait 5
+echo "...Done."
+echo "Making makv2xbox executable..."
+chmod +x common/makev2xbox
+echo "Copying wpa_supplicant-wlan0.conf..."
+sudo cp common/wpa_supplicant/v2x/wpa_supplicant-wlan0.conf /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+echo "Copying wpa_supplicant-wlan1.conf..."
+sudo cp common/wpa_supplicant/v2x/wpa_supplicant-wlan1.conf /etc/wpa_supplicant/wpa_supplicant-wlan1.conf
+echo "Copying rc.local..."
+sudo cp common/rc.local /etc/rc.local
+echo "Copying rc.local..."
+echo "OwlBox-V2X Install Complete!"
