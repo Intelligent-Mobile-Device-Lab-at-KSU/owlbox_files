d9f18cf10ce20b1f8aa86c2c26c89583c7b9cd8f
scrappylabs <57416600+scrappylabs@users.noreply.github.com>
2023-05-24 03:39:47 -0400

    Update bootdsrctx.py

--- test/bootdsrctx.py
+++ test/bootdsrctx.py
@@ -3,9 +3,27 @@ from scapy.all import hexdump, raw, bytes_encode
 from scapy.layers.l2 import Ether, Dot1Q
 import time
 from datetime import datetime, timedelta
+import logging
+import threading
+import subprocess
+
+def countfiles():
+    fcount=0
+    for fname in os.listdir('/home/pi/owlbox_files/logs'):
+        fpath=os.path.join('/home/pi/owlbox_files/logs',fname)
+        if os.path.isfile(fpath):
+            fcount+=1
+    
+    return fcount
+
+doLog = False
+if countfiles == 0:
+    noLogFilePresent = True
+else:
+    noLogFilePresent = False
 
 # Get the host name
-#host_name = socket.gethostname()
+host_name = socket.gethostname()
 
 # TCP server details
 #server_address = ('192.168.1.3', 4030)
@@ -53,14 +71,56 @@ s.bind(("wlan0", 0))
 #message = dst_addr+src_addr+ethertype+payload
 encoded = bytes_encode(pkt)
 
+traffictype = "video"
+
+def dellogs():
+    bash_cmd = f"rm -f /home/pi/owlbox_files/logs/*"
+    try:
+        subprocess.run(bash_cmd,shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print("Error Occured: {e}")
+    
+def checkServer():
+    global doLog
+    while True:
+        cs = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        server_address = ('192.168.1.4', 8001)
+        cs.sendto("checklog-{}".format(host_name).encode('utf-8'),server_address)
+        r = cs.recvfrom(1024)
+        rm = r[0].decode('utf-8')
+        if rm == "log":
+            doLog=True
+            current_time = datetime.now().strftime('%m_%d_%Y_%H_%M')
+            logging.basicConfig(filename="/home/pi/owlbox_files/logs/{}_{}.log".format(host_name,current_time), level=logging.DEBUG, format='%(asctime)s.%(msecs)03d %(levelname)s %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
+            noLogFilePresent = False
+        elif rm == "nolog":
+            doLog=False
+        elif rm == "logdeletealllogs":
+            dellogs()
+            current_time = datetime.now().strftime('%m_%d_%Y_%H_%M')
+            logging.basicConfig(filename="/home/pi/owlbox_files/logs/{}_{}.log".format(host_name,current_time), level=logging.DEBUG, format='%(asctime)s.%(msecs)03d %(levelname)s %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
+            noLogFilePresent = False
+            doLog=True
+        elif rm=="nologdeletealllogs":
+            doLog=False
+            dellogs()
+            noLogFilePresent = True
+            
+        time.sleep(1)
+
+x = threading.Thread(target=checkServer)
+x.start()
 while True:
      s.send(encoded)
+     if doLog:
+         logging.debug("{}-{}".format(len(encoded),traffictype))
      # Get the current date and time
      #current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
      # Combine the host name and date-time into a string
      #message = f"{host_name},{current_time}"
      # Send the message to the server
      #client_socket.sendall(message.encode())
+     #time.sleep(.000001)
 
 # Close the socket
 #client_socket.close()
