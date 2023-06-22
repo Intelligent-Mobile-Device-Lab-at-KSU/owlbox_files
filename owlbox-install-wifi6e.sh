f821622c465fdf4029c0f484fcb65928fdfe42db
scrappylabs <57416600+scrappylabs@users.noreply.github.com>
2023-06-21 20:14:58 -0400

    Update owlbox-install-wifi6e.sh

--- owlbox-install-wifi6e.sh
+++ owlbox-install-wifi6e.sh
@@ -1,1 +1,12 @@
-a
+#!/bin/bash
+echo "Changing to /home/pi/owlbox_files"
+cd /home/pi/owlbox_files
+rm /home/pi/owlbox_files/logs/_blank.txt
+echo "...Done."
+echo "Copying wpa_supplicant-wlan0.conf..."
+sudo cp common/wpa_supplicant/wifi6e/wpa_supplicant-wlan0.conf /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+echo "Copying wpa_supplicant-wlan1.conf..."
+sudo cp common/wpa_supplicant/wifi6e/wpa_supplicant-wlan1.conf /etc/wpa_supplicant/wpa_supplicant-wlan1.conf
+echo "Copying rc.local..."
+sudo cp common/rcwifi6e.local /etc/rc.local
+echo "OwlBox-WiFi6e Install Complete!"
